bb8db0b6fb04a866725850821fad93f64032af7d
elizeurs <13ders@gmail.com>
2019-10-08 17:13:47 -0300

    setup project with two targets, UI for login and register, code for register a new user

---
+++ Shared/Utils/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  Artable
+//
+//  Created by Elizeu RS on 04/10/19.
+//  Copyright © 2019 elizeurs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Storyboard {
+  static let LoginStoryboard = "LoginStoryboard"
+  static let Main = "Main"
+}
+
+struct StoryboardId {
+  static let LoginVC = "loginVC"
+}
+
+struct AppImages {
+  static let GreenCheck = "green_check"
+  static let RedCheck = "red_check"
+}
+
+struct AppColors {
+  static let Blue = #colorLiteral(red: 0.2914361954, green: 0.3395442367, blue: 0.4364506006, alpha: 1)
+  static let Red = #colorLiteral(red: 0.8739202619, green: 0.4776076674, blue: 0.385545969, alpha: 1)
+  static let OffWhite = #colorLiteral(red: 0.9626371264, green: 0.959995091, blue: 0.9751287103, alpha: 1)
+}
---
+++ Shared/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Artable
+//
+//  Created by Elizeu RS on 04/10/19.
+//  Copyright © 2019 elizeurs. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  var isNotEmpty : Bool {
+    return !isEmpty
+  }
+}
